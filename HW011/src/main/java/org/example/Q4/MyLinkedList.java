7b2b274773d962c90faf47410159a827a961e6bf
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2023-04-20 04:01:23 +0330

    feat: [Q4] MyLinkedList made

---
+++ HW011/src/main/java/org/example/Q4/MyLinkedList.java
@@ -0,0 +1,106 @@
+package org.example.Q4;
+
+public class MyLinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void add(int index, T data) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> newNode = new Node<>(data);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void remove(T data) {
+        if (head == null) {
+            return;
+        }
+        if (head.data.equals(data)) {
+            head = head.next;
+            size--;
+            return;
+        }
+        Node<T> current = head;
+        while (current.next != null && !current.next.data.equals(data)) {
+            current = current.next;
+        }
+        if (current.next != null) {
+            current.next = current.next.next;
+            size--;
+        }
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        size--;
+    }
+
+    public int indexOf(T data) {
+        int index = 0;
+        Node<T> current = head;
+        while (current != null && !current.data.equals(data)) {
+            current = current.next;
+            index++;
+        }
+        if (current == null) {
+            return -1;
+        }
+        return index;
+    }
+}
